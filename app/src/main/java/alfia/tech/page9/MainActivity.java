1e9f46514e50ff2265b8be75ae6e17c5a3f979bc
Alfia <ppunnaleul@gmail.com>
2020-12-02 06:35:50 +1345

    First Commit

---
+++ app/src/main/java/alfia/tech/page9/MainActivity.java
@@ -0,0 +1,14 @@
+package alfia.tech.page9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
